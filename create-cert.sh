3b7e072603aad82a2e0523524a711d7c7f183681
Aapeli Haanpuu <aapeli.haanpuu@hoxhunt.com>
2022-08-24 17:38:19 +0300

    Add Dockerfile and update README

---
+++ create-cert.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+openssl req -x509 -new -newkey rsa:2048 -nodes -subj '/C=US/ST=California/L=San Francisco/O=JankyCo/CN=Test Identity Provider' -keyout idp-private-key.pem -out idp-public-cert.pem -days 7300
